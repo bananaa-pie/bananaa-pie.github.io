54752ebf21e5bc00f61759c88be49825301b361c
bananaa-pie <106494010+bananaa-pie@users.noreply.github.com>
2022-05-30 13:48:15 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,14 +16,14 @@ let myHeading = document.querySelector('h1')
 function setUserName(){
     let myName = prompt('Please enter your name')
     localStorage.setItem('name', myName)
-    myHeading.textContent = 'Mozilla is cool, ' + myName
+    myHeading.textContent = 'Banana pie is cool, ' + myName
 }
 
 if(!localStorage.getItem('name')){
     setUserName()
 } else{
     let storedName = localStorage.getItem('name')
-    myHeading.textContent = 'Mozilla is cool, ' + storedName
+    myHeading.textContent = 'Banana pie is cool, ' + storedName
 }
 
 myButton.onclick = function () {
